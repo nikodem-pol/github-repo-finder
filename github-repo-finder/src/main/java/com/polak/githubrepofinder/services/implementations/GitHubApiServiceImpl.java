b471467b724173833b5a53889d26c5edf17c4385
nikodem-pol <nw.polak@gmail.com>
2024-02-17 21:11:32 +0100

    Implement getUserRepositories method

--- github-repo-finder/src/main/java/com/polak/githubrepofinder/services/implementations/GitHubApiServiceImpl.java
+++ github-repo-finder/src/main/java/com/polak/githubrepofinder/services/implementations/GitHubApiServiceImpl.java
@@ -1,15 +1,75 @@
 package com.polak.githubrepofinder.services.implementations;
 
+import com.polak.githubrepofinder.dtos.BranchDto;
 import com.polak.githubrepofinder.dtos.RepositoriesResponse;
+import com.polak.githubrepofinder.dtos.RepositoryDto;
+import com.polak.githubrepofinder.exceptions.CannotGetRepoBranchesException;
+import com.polak.githubrepofinder.exceptions.ConnectionFailedException;
+import com.polak.githubrepofinder.exceptions.UserNotFoundException;
 import com.polak.githubrepofinder.services.interfaces.GitHubApiService;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class GitHubApiServiceImpl implements GitHubApiService {
+    private final GitHub github;
+
+    public GitHubApiServiceImpl() throws ConnectionFailedException {
+        try {
+            this.github = GitHub.connectAnonymously();
+        } catch (IOException e) {
+            throw new ConnectionFailedException("Failed to connect to GitHub.");
+        }
+    }
+
     @Override
-    public RepositoriesResponse getUserRepositories(String username) {
-        return null;
+    public RepositoriesResponse getUserRepositories(String username) throws UserNotFoundException {
+        try {
+            GHUser user = github.getUser(username);
+            return RepositoriesResponse.builder().repositories(getNotForkRepositories(user)).build();
+        } catch (IOException e) {
+            throw new UserNotFoundException(String.format("User with name: {%s}, not found", username));
+        }
+    }
+
+    private List<RepositoryDto> getNotForkRepositories(GHUser user) throws IOException {
+        List<GHRepository> notForkRepos = user.getRepositories()
+                                              .values()
+                                              .stream()
+                                              .filter(repo -> !repo.isFork())
+                                              .toList();
+
+        return notForkRepos.stream().map(this::convertGHRepositoryToRepositoryDto).toList();
+    }
+
+
+    private RepositoryDto convertGHRepositoryToRepositoryDto(GHRepository repository) {
+        List<BranchDto> branches;
+        branches = getRepositoryBranchesDtos(repository);
+        return RepositoryDto.builder()
+                            .name(repository.getName())
+                            .ownerLogin(repository.getOwnerName())
+                            .branches(branches)
+                            .build();
+    }
+
+    private List<BranchDto> getRepositoryBranchesDtos(GHRepository repository) {
+        try {
+            return repository.getBranches()
+                             .values()
+                             .stream()
+                             .map(branch -> BranchDto.builder()
+                                                     .name(branch.getName())
+                                                     .lastCommitSha(branch.getSHA1())
+                                                     .build())
+                             .toList();
+        } catch (IOException e) {
+            throw new CannotGetRepoBranchesException(e.getMessage());
+        }
     }
 }
--- github-repo-finder/src/main/java/com/polak/githubrepofinder/services/interfaces/GitHubApiService.java
+++ github-repo-finder/src/main/java/com/polak/githubrepofinder/services/interfaces/GitHubApiService.java
@@ -1,9 +1,11 @@
 package com.polak.githubrepofinder.services.interfaces;
 
 import com.polak.githubrepofinder.dtos.RepositoriesResponse;
+import com.polak.githubrepofinder.exceptions.UserNotFoundException;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface GitHubApiService {
-    RepositoriesResponse getUserRepositories(String username);
+    RepositoriesResponse getUserRepositories(String username) throws UserNotFoundException;
 }
