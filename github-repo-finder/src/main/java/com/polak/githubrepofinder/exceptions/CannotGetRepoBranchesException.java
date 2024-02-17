543aa93cbe0858d5d0d47d501a510165314d4f99
nikodem-pol <nw.polak@gmail.com>
2024-02-17 21:10:26 +0100

    Create CannotGetRepoBranchesException class

---
+++ github-repo-finder/src/main/java/com/polak/githubrepofinder/exceptions/CannotGetRepoBranchesException.java
@@ -0,0 +1,5 @@
+package com.polak.githubrepofinder.exceptions;
+
+public class CannotGetRepoBranchesException extends RuntimeException {
+    public CannotGetRepoBranchesException(String msg) { super(msg); }
+}
