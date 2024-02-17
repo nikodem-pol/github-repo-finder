bed6730e4556a5b2ad0eb39cd60f28a49599cd7b
nikodem-pol <nw.polak@gmail.com>
2024-02-17 16:34:59 +0100

    Init Spring Boot project

---
+++ github-repo-finder/src/main/java/com/polak/githubrepofinder/GtiHubRepositoriesFinderApplication.java
@@ -0,0 +1,13 @@
+package com.polak.githubrepofinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtiHubRepositoriesFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtiHubRepositoriesFinderApplication.class, args);
+	}
+
+}
