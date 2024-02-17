b543e8e18c583f43fe7187e27d63f6e8f792ec4b
nikodem-pol <nw.polak@gmail.com>
2024-02-17 21:34:32 +0100

    Handle request validation exceptions

--- github-repo-finder/src/main/java/com/polak/githubrepofinder/controllers/GitHubController.java
+++ github-repo-finder/src/main/java/com/polak/githubrepofinder/controllers/GitHubController.java
@@ -2,23 +2,23 @@ package com.polak.githubrepofinder.controllers;
 
 import com.polak.githubrepofinder.dtos.RepositoriesRequest;
 import com.polak.githubrepofinder.dtos.RepositoriesResponse;
+import com.polak.githubrepofinder.exceptions.UserNotFoundException;
 import com.polak.githubrepofinder.services.interfaces.GitHubApiService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("api/v1/github")
+
+@RestController
+@RequestMapping("/api/v1/github")
 @RequiredArgsConstructor
 public class GitHubController {
     private final GitHubApiService gitHubApiService;
 
     @PostMapping(value = "/repos", consumes = "application/json")
     @ResponseStatus(HttpStatus.OK)
-    public RepositoriesResponse getUsersRepositories(@RequestBody @Valid RepositoriesRequest request) {
+    public RepositoriesResponse getUsersRepositories(@RequestBody @Valid RepositoriesRequest request) throws UserNotFoundException {
         return gitHubApiService.getUserRepositories(request.getUsername());
     }
 }
--- github-repo-finder/src/main/java/com/polak/githubrepofinder/exceptions/DefaultExceptionHandler.java
+++ github-repo-finder/src/main/java/com/polak/githubrepofinder/exceptions/DefaultExceptionHandler.java
@@ -2,9 +2,13 @@ package com.polak.githubrepofinder.exceptions;
 
 import com.polak.githubrepofinder.dtos.ErrorResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class DefaultExceptionHandler {
@@ -19,5 +23,16 @@ public class DefaultExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleApiExceptions(UserNotFoundException exception) {
         return ErrorResponse.builder().message(exception.getMessage()).status("500").build();
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<String> handleValidationException(MethodArgumentNotValidException exception) {
+        return exception
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
     }
 }
