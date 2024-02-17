9d1dc365e3ac087d1e9f2a355740991452ee8894
nikodem-pol <nw.polak@gmail.com>
2024-02-17 17:22:32 +0100

    Create GitHubApiServiceImpl service

---
+++ github-repo-finder/src/main/java/com/polak/githubrepofinder/services/implementations/GitHubApiServiceImpl.java
@@ -0,0 +1,8 @@
+package com.polak.githubrepofinder.services.implementations;
+
+import com.polak.githubrepofinder.services.interfaces.GitHubApiService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GitHubApiServiceImpl implements GitHubApiService {
+}
