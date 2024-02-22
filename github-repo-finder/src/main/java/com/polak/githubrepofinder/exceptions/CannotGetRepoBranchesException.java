94fae20d931be1d6243233dc9ccfa3de4442d384
nikodem-pol <nw.polak@gmail.com>
2024-02-22 17:12:53 +0100

    Delete CannotGetRepoBranchesException.class

--- github-repo-finder/src/main/java/com/polak/githubrepofinder/exceptions/CannotGetRepoBranchesException.java
+++
@@ -1,5 +0,0 @@
-package com.polak.githubrepofinder.exceptions;
-
-public class CannotGetRepoBranchesException extends RuntimeException {
-    public CannotGetRepoBranchesException(String msg) { super(msg); }
-}
