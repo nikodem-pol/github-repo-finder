54d1b6571cf5343927738237c960ba224286b21d
nikodem-pol <nw.polak@gmail.com>
2024-02-22 11:45:14 +0100

    Configure WebClient

--- github-repo-finder/src/main/java/com/polak/githubrepofinder/configuration/GitHubApiClientConfig.java
+++ github-repo-finder/src/main/java/com/polak/githubrepofinder/configuration/GitHubApiClientConfig.java
@@ -1,18 +1,15 @@
 package com.polak.githubrepofinder.configuration;
 
 import lombok.extern.slf4j.Slf4j;
-import org.kohsuke.github.GitHub;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.io.IOException;
-
 @Configuration
 @Slf4j
-public class GitHubConfig {
+public class GitHubApiClientConfig {
     @Bean
     WebClient github() {
-        return WebClient.create("https://");
+        return WebClient.create("https://api.github.com");
     }
 }
