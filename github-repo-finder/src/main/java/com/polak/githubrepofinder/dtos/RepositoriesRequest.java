e6b523e5565317bd8154f0c39ce07dc5a1a9ead7
nikodem-pol <nw.polak@gmail.com>
2024-02-24 00:42:39 +0100

    Change RepositoryRequest to be a record

--- github-repo-finder/src/main/java/com/polak/githubrepofinder/dtos/RepositoriesRequest.java
+++ github-repo-finder/src/main/java/com/polak/githubrepofinder/dtos/RepositoriesRequest.java
@@ -2,15 +2,9 @@ package com.polak.githubrepofinder.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class RepositoriesRequest {
-    @NotNull(message = "username is null")
-    @NotBlank(message = "username is blank or empty")
-    private String username;
-}
+public record RepositoriesRequest(
+        @NotNull(message = "username is null")
+        @NotBlank(message = "username is blank or empty")
+        String username)
+{}
