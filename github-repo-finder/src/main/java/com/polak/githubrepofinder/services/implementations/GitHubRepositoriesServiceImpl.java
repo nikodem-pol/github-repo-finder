00d3f41df5d7a7b82abd193572d4476471d1e473
nikodem-pol <nw.polak@gmail.com>
2024-02-23 23:44:26 +0100

    Use GitHubApiService

--- github-repo-finder/src/main/java/com/polak/githubrepofinder/services/implementations/GitHubRepositoriesServiceImpl.java
+++ github-repo-finder/src/main/java/com/polak/githubrepofinder/services/implementations/GitHubRepositoriesServiceImpl.java
@@ -1,17 +1,12 @@
 package com.polak.githubrepofinder.services.implementations;
 
 import com.polak.githubrepofinder.dtos.BranchDto;
-import com.polak.githubrepofinder.dtos.GitHubBranch;
-import com.polak.githubrepofinder.dtos.GitHubRepository;
 import com.polak.githubrepofinder.dtos.RepositoryResponse;
-import com.polak.githubrepofinder.exceptions.UserNotFoundException;
+import com.polak.githubrepofinder.services.interfaces.GitHubApiService;
 import com.polak.githubrepofinder.services.interfaces.GitHubRepositoriesService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
@@ -19,12 +14,12 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class GitHubRepositoriesServiceImpl implements GitHubRepositoriesService {
-    private final WebClient githubClient;
+    private final GitHubApiService gitHubApi;
 
     @Override
     public Flux<RepositoryResponse> getUserRepositories(String username) {
-        return getNonForkRepositories(username).publishOn(Schedulers.boundedElastic()).map(repo -> {
-            List<BranchDto> branches = getRepositoryBranches(repo.owner().login(), repo.name())
+        return gitHubApi.getNonForkRepositories(username).publishOn(Schedulers.boundedElastic()).map(repo -> {
+            List<BranchDto> branches = gitHubApi.getRepositoryBranches(repo.owner().login(), repo.name())
                                                             .map(branch -> BranchDto.builder()
                                                                                     .name(branch.name())
                                                                                     .lastCommitSha(branch.commit().sha())
@@ -38,25 +33,6 @@ public class GitHubRepositoriesServiceImpl implements GitHubRepositoriesService 
                                       .branches(branches)
                                       .build();
         });
-    }
-
-    private Flux<GitHubRepository> getNonForkRepositories(String username) {
-        return
-                githubClient.get()
-                           .uri("/users/{username}/repos", username)
-                           .retrieve()
-                           .onStatus(HttpStatus.NOT_FOUND::equals, response -> Mono.just(new UserNotFoundException(String.format("User with username: {%s} not found.", username))))
-                           .bodyToFlux(GitHubRepository.class)
-                           .filter(repo -> !repo.fork());
-
-    }
-
-    private Flux<GitHubBranch> getRepositoryBranches(String username, String repoName) {
-        return
-                githubClient.get()
-                           .uri("/repos/{username}/{repoName}/branches", username, repoName)
-                           .retrieve()
-                           .bodyToFlux(GitHubBranch.class);
     }
 
 }
