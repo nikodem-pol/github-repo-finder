e4b63fc786f9ca60a792ff09b2bf30e7fce8c8ca
nikodem-pol <nw.polak@gmail.com>
2024-02-24 00:09:58 +0100

    Change RepositoryResponse to be a record

--- github-repo-finder/src/main/java/com/polak/githubrepofinder/dtos/RepositoryResponse.java
+++ github-repo-finder/src/main/java/com/polak/githubrepofinder/dtos/RepositoryResponse.java
@@ -1,18 +1,9 @@
 package com.polak.githubrepofinder.dtos;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class RepositoryResponse {
-    String name;
-    String ownerLogin;
-    List<BranchDto> branches;
+public record RepositoryResponse(String name, String ownerLogin, List<BranchDto> branches) {
 }
