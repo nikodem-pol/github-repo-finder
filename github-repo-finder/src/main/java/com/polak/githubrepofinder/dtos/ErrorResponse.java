6c22cfa5c3ad9e57f6bd995c637468f555d74c18
nikodem-pol <nw.polak@gmail.com>
2024-02-24 21:57:20 +0100

    Change ErrorResponse to be a record

--- github-repo-finder/src/main/java/com/polak/githubrepofinder/dtos/ErrorResponse.java
+++ github-repo-finder/src/main/java/com/polak/githubrepofinder/dtos/ErrorResponse.java
@@ -1,15 +1,7 @@
 package com.polak.githubrepofinder.dtos;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ErrorResponse {
-    String status;
-    String message;
+public record ErrorResponse(String status, String message) {
 }
